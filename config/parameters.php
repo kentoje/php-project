a05c53893687600976644553d6390888722b0c5f
Kento <k.monthubert@gmail.com>
2019-05-27 14:41:50 +0200

    Create parameters.php

---
+++ config/parameters.php
@@ -0,0 +1,10 @@
+<?php 
+
+// paramètres PDO
+const DB_SGBD   = 'mysql';
+const DB_HOST   = 'localhost';
+/* TODO: Rajouter le nom de la DB */
+const DB_DBNAME = '';
+/* Config MAMP, might change */
+const DB_USER   = 'root';
+const DB_PASS   = 'root';
