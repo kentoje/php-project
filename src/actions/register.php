7ade37422e5d473ef2b9b4f51c965033ea1bfba7
kentoje <k.monthubert@gmail.com>
2019-05-29 01:03:37 +0200

    add signup and register page

---
+++ src/actions/register.php
@@ -0,0 +1,59 @@
+<?php 
+/* TODO: Revoir la sécurité, inexistantes. */
+
+require_once '../config/bootstrap.php';
+
+/* Variables */
+$email = '';
+$username = '';
+$errors = array();
+
+/* Connection to DB */
+$db = App\Database::connect_database();
+/* var_dump($db); */
+
+/* Registration */
+if ( isset( $_POST['submit'] ) ) {
+  $email = $_POST['email'];
+  $username = $_POST['name'];
+  $password = password_hash( $_POST['password'], PASSWORD_DEFAULT );
+
+  if ( empty($email) ) {
+    array_push( $errors, 'Vous devez mettre un email !' );
+  }
+
+  if ( empty($username) ) {
+    array_push( $errors, 'Vous devez mettre un pseudo !' );
+  }
+
+  if ( empty($password) ) {
+    array_push( $errors, 'Vous devez mettre un mot de passe !' );
+  }
+
+  /* Check DB if user exists */
+  $userCheckQuery = "SELECT * FROM users WHERE pseudo = :name OR email = :email LIMIT 1";
+  $req = App\Database::$pdo->prepare( $userCheckQuery );
+  $req->bindParam( ':name', $_POST['name'] );
+  $req->bindParam( ':email', $_POST['email'] );
+  $req->execute();
+
+  $user = $req->fetch( PDO::FETCH_ASSOC );
+
+  if ($user) {
+    if ( $user['pseudo'] == $username ) {
+      array_push( $errors, 'Ce pseudo existe déjà !' );
+    }
+
+    if ( $user['email'] == $email ) {
+      array_push( $errors, 'Ce mail existe déjà !' );
+    }
+  }
+
+  if ( count($errors) == 0 ) {
+    $query = "INSERT INTO users(pseudo, email, password, photo) VALUES ('$username', '$email', '$password', '')";
+    $insertReq = App\Database::$pdo->exec( $query );
+    $_SESSION['name'] = $username;
+    $_SESSION['success'] = 'Vous êtes connecté !';
+    header( 'location: ../index.php' );
+  }
+}
---
+++ src/pages/signup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Sign Up</title>
+</head>
+<body>
+  <?php 
+    require_once '../config/bootstrap.php';
+  ?>
+  <form method="post" action="../actions/register.php">
+    <label for="email">E-mail</label>
+    <input type="text" name="email">
+    <label for="name">Nom</label>
+    <input type="text" name="name">
+    <label for="password">Mot de passe</label>
+    <input type="text" name="password">
+    <button type="submit" name="submit">Submit</button>
+  </form>
+</body>
+</html>
